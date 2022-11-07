ba86212047ff16579c5152d22b2acc8cc346cc90
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-11-07 20:31:38 -0300

    db: base para o banco de dados estatico

---
+++ db.js
@@ -0,0 +1,62 @@
+const filmList = [
+  {
+    "id": 1,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 2,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 3,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 4,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 5,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 6,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 7,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 8,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 9,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  },
+  {
+    "id": 10,
+    "title": "film",
+    "platform": "platform",
+    "imdb": "x",
+  }
+];
