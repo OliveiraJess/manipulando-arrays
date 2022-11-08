1d5b3f54917e01dbd3f364af5dc5375cc753cdbf
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-11-08 19:13:46 -0300

    js: passando o db para o main

---
+++ main.js
@@ -0,0 +1,64 @@
+const dbFilm = [
+  {
+    "id": 1,
+    "title": "o sexto sentido ",
+    "genre": "suspense",
+    "imdb": "8,2",
+  },
+  {
+    "id": 2,
+    "title": "o exorcista",
+    "genre": "terror",
+    "imdb": "8,1",
+  },
+  {
+    "id": 3,
+    "title": "os outros",
+    "genre": "terror",
+    "imdb": "7,6",
+  },
+  {
+    "id": 4,
+    "title": "efeito borboleta",
+    "genre": "ficção científica",
+    "imdb": "7,6",
+  },
+  {
+    "id": 5,
+    "title": "o orfanato",
+    "genre": "terror",
+    "imdb": "7,4",
+  },
+  {
+    "id": 6,
+    "title": "o chamado",
+    "genre": "terror",
+    "imdb": "7,1",
+  },
+  {
+    "id": 7,
+    "title": "a órfã",
+    "genre": "terror",
+    "imdb": "7,0",
+  },
+  {
+    "id": 8,
+    "title": "espíritos: a morte está ao seu lado ",
+    "genre": "terror",
+    "imdb": "7,0",
+  },
+  {
+    "id": 9,
+    "title": "paranóia ",
+    "genre": "suspense",
+    "imdb": "6,8",
+  },
+  {
+    "id": 10,
+    "title": "arraste-me para o inferno",
+    "genre": "terror",
+    "imdb": "6,6",
+  },
+];
+
+console.log(dbFilm)
